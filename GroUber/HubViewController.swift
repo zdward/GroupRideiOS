fbf52716155fc6364e0728920d00ac1a8b8e6707
alex chuckas <alexchuckas@ri-87-rsdh-wl-357.dynamic.rpi.edu>
2019-01-25 16:59:18 -0500

    1) added the hub view controller 2) segue from main view controller to hub 3) login added, but no server control, so cannot find user, entering any username and password will transition to hub

---
+++ GroUber/HubViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HubViewController.swift
+//  GroUber
+//
+//  Created by alex chuckas on 1/25/19.
+//  Copyright © 2019 Zachary Ward. All rights reserved.
+//
+
+import UIKit
+
+class HubViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- GroUber/ViewController.swift
+++ GroUber/ViewController.swift
@@ -9,12 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var username_field: UITextField!
+    @IBOutlet weak var password_field: UITextField!
+    
+    var activityIndicator = UIActivityIndicatorView()
+    var signup_mode = true; //WILL SAVE THIS IN USERDEFAULTS LATER, saved pass and username feature, maybe attempt autologin
+    
+    //displays an alert to the view controller
+    func createAlert(title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            self.dismiss(animated: true, completion: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    //when the login button is pressed
+    @IBAction func Login_Button_Action(_ sender: Any) {
+        if username_field.text == "" || password_field.text == "" {
+            createAlert(title: "ERROR", message: "Invalid Username/Password")
+            
+        } else {
+            
+            //buffering, cannot interact with screen
+            activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            activityIndicator.center = self.view.center
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.style = UIActivityIndicatorView.Style.gray
+            view.addSubview(activityIndicator)
+            activityIndicator.startAnimating()
+            UIApplication.shared.beginIgnoringInteractionEvents()
+            
+            
+            //this will be the code to get the username and password from the server and then login to their profile
+            if signup_mode {
+                //create a user utilizing the database
+                self.activityIndicator.stopAnimating() //temp
+                UIApplication.shared.endIgnoringInteractionEvents() //temp
+                performSegue(withIdentifier: "Login_Segue", sender: nil)
+            } else {
+                //fetch for user and if exists, login
+                self.activityIndicator.stopAnimating() //temp
+                UIApplication.shared.endIgnoringInteractionEvents() //temp
+                performSegue(withIdentifier: "Login_Segue", sender: nil)
+            }
+            
+        }
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
