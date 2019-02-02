cc5e51a5be668f68522807c55c5b3140428ceed1
Alexis Joseph <alexisjoseph@graben-142.dynamic2.rpi.edu>
2019-02-02 15:09:24 -0500

    firebase import now works, documentation for this coming soon

---
+++ Pods/Target Support Files/Pods-GroUber/Pods-GroUber-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GroUber : NSObject
+@end
+@implementation PodsDummy_Pods_GroUber
+@end
---
+++ Pods/Target Support Files/Pods-GroUberUITests/Pods-GroUberUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GroUberUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GroUberUITests
+@end
