6aa51d9dd4694ff36b1d1345836152194065f613
Alexis Joseph <alexisjoseph@squat-58.dynamic2.rpi.edu>
2019-02-26 00:42:07 -0500

    message page wip

--- GroUber/FeedCell.swift
+++ GroUber/FeedCell.swift
@@ -9,8 +9,89 @@
 import UIKit
 
 class FeedCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var messageText: UITextView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var totalVotesLabel: UILabel!
+    @IBOutlet weak var thumbVoteImage: UIImageView!
+    
+    var message: Message!
+    var voteRef: Firebase!
+    
+    func configureCell(message: Message) {
+        self.message = message
+        
+        // Set the labels and textView.
+        
+        self.messageText.text = message.messageText
+        self.totalVotesLabel.text = "Total Votes: \(message.messageVotes)"
+        self.usernameLabel.text = message.username
+        
+        // Set "votes" as a child of the current user in Firebase and save the message's key in votes as a boolean.
+        
+        voteRef = DataService.dataService.CURRENT_USER_REF.childByAppendingPath("votes").childByAppendingPath(message.jokeKey)
+        
+        // observeSingleEventOfType() listens for the thumb to be tapped, by any user, on any device.
+        
+        voteRef.observeSingleEventOfType(.Value, withBlock: { snapshot in
+            
+            // Set the thumb image.
+            
+            if let thumbsUpDown = snapshot.value as? NSNull {
+                
+                // Current user hasn't voted for the message... yet.
+                
+                print(thumbsUpDown)
+                self.thumbVoteImage.image = UIImage(named: "thumb-down")
+            } else {
+                
+                // Current user voted for the message!
+                
+                self.thumbVoteImage.image = UIImage(named: "thumb-up")
+            }
+        })
+    }
+    
+    func voteTapped(sender: UITapGestureRecognizer) {
+        
+        // observeSingleEventOfType listens for a tap by the current user.
+        
+        voteRef.observeSingleEventOfType(.Value, withBlock: { snapshot in
+            
+            if let thumbsUpDown = snapshot.value as? NSNull {
+                print(thumbsUpDown)
+                self.thumbVoteImage.image = UIImage(named: "thumb-down")
+                
+                // addSubtractVote(), in message.swift, handles the vote.
+                
+                self.message.addSubtractVote(true)
+                
+                // setValue saves the vote as true for the current user.
+                // voteRef is a reference to the user's "votes" path.
+                
+                self.voteRef.setValue(true)
+            } else {
+                self.thumbVoteImage.image = UIImage(named: "thumb-up")
+                self.message.addSubtractVote(false)
+                self.voteRef.removeValue()
+            }
+            
+        })
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // UITapGestureRecognizer is set programatically.
+        
+        let tap = UITapGestureRecognizer(target: self, action: "voteTapped:")
+        tap.numberOfTapsRequired = 1
+        thumbVoteImage.addGestureRecognizer(tap)
+        thumbVoteImage.userInteractionEnabled = true
+    }
 
-    @IBOutlet weak var ProfileImage: UIImageView!
+    /*@IBOutlet weak var ProfileImage: UIImageView!
     @IBOutlet weak var MessageLabel: UILabel!
     @IBOutlet weak var ProfileName: UILabel!
     
@@ -18,7 +99,7 @@ class FeedCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
+    } */
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
--- GroUber/message.swift
+++ GroUber/message.swift
@@ -6,3 +6,69 @@
 //
 
 import Foundation
+import Firebase
+
+class Joke {
+    private var _messageRef: Firebase!
+    
+    private var _messageKey: String!
+    private var _messageText: String!
+    private var _messageVotes: Int!
+    private var _username: String!
+    
+    var messageKey: String {
+        return _messageKey
+    }
+    
+    var messageText: String {
+        return _messageText
+    }
+    
+    var messageVotes: Int {
+        return _messageVotes
+    }
+    
+    var username: String {
+        return _username
+    }
+    
+    // Initialize the new Message
+    
+    init(key: String, dictionary: Dictionary<String, AnyObject>) {
+        self._messageKey = key
+        
+        // Within the Message, or Key, the following properties are children
+        
+        if let votes = dictionary["votes"] as? Int {
+            self._messageVotes = votes
+        }
+        
+        if let message = dictionary["messageText"] as? String {
+            self._messageText = joke
+        }
+        
+        if let user = dictionary["author"] as? String {
+            self._username = user
+        } else {
+            self._username = ""
+        }
+        
+        // The above properties are assigned to their key.
+        
+        self._messageRef = DataService.dataService.MESSAGE_REF.childByAppendingPath(self._messageKey)
+    }
+    
+    func addSubtractVote(addVote: Bool) {
+        
+        if addVote {
+            _messageVotes = _messageVotes + 1
+        } else {
+            _messageVotes = _messageVotes - 1
+        }
+        
+        // Save the new vote total.
+        
+        _messageRef.childByAppendingPath("votes").setValue(_messageVotes)
+        
+    }
+}
