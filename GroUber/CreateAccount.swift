499e73030f0adeafdbc61b72b77d060f0c35f5fb
Farukh Saidmuratov <saidmf@rpi.edu>
2019-03-12 17:15:46 -0400

    made regsiter error message handle more cases

--- GroUber/CreateAccount.swift
+++ GroUber/CreateAccount.swift
@@ -29,19 +29,25 @@ class CreateAccount: UIViewController{
     @IBAction func sign_in(_ sender: Any) {
         var email: String = String(RCSID.text!);
         var keyword: String;
+        
         if password.text == confirmed_pass.text {
             keyword = String(password.text!)
         }
         else{
             keyword = "NULL" //return an error in this case
         }
+        
         email = email + "@rpi.edu";
         keyword = keyword + "_" //so the outlet is used, really a useless line of code
-        if email == "" || keyword == "" || displayName.text == ""{
-            createAlert(title: "ERROR", message: "Please fill out all fields");
+        
+        if email == "" || keyword == "" || displayName.text == "" || confirmed_pass.text == ""{
+            createAlert(title: "Error", message: "Please fill out all fields");
+            return;
+        }  else if String(password.text!).count < 6 {
+            createAlert(title: "Error", message: "Password must be at least 6 characters long");
             return;
         } else if password.text != confirmed_pass.text {
-            createAlert(title: "ERROR", message: "Please make sure that passwords match");
+            createAlert(title: "Error", message: "Please make sure that passwords match");
             
             password.layer.cornerRadius = 8.0;
             password.layer.masksToBounds = true;
@@ -56,9 +62,20 @@ class CreateAccount: UIViewController{
             return;
         }
         else{
-            password.layer.borderColor = UIColor.clear.cgColor;
+            confirmed_pass.layer.cornerRadius = 8.0;
+            confirmed_pass.layer.masksToBounds = true;
             confirmed_pass.layer.borderColor = UIColor.clear.cgColor;
+            confirmed_pass.layer.borderWidth = 1.0;
+            
+            password.layer.cornerRadius = 8.0;
+            password.layer.masksToBounds = true;
+            password.layer.borderColor = UIColor.clear.cgColor;
+            password.layer.borderWidth = 1.0;
+            
+            displayName.layer.cornerRadius = 8.0;
+            displayName.layer.masksToBounds = true;
             displayName.layer.borderColor = UIColor.clear.cgColor;
+            displayName.layer.borderWidth = 1.0;
             
             //Stores the new user on firebase/authentication
             Auth.auth().createUser(withEmail: email, password: keyword) { authResult, error in
