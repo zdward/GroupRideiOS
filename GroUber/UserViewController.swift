b14e533718b64969c8d95c54c4b1ccf4b2766545
Farukh Saidmuratov <saidmf@rpi.edu>
2019-03-16 23:50:47 -0400

    Made fields highlight when incorrect and upload user image

---
+++ GroUber/UserViewController.swift
@@ -0,0 +1,48 @@
+//
+//  UserViewController.swift
+//  GroUber
+//
+//  Created by Farukh Saidmuratov on 3/16/19.
+//  Copyright © 2019 Zachary Ward. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet var userImage: UIImageView!
+    
+    @IBAction func updateUserImage(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        imagePicker.allowsEditing = false
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            userImage.image = image
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
